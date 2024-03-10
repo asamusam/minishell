d53fcf63096241eff64174f7045c64a3680b55c5
Manfredo Mugheddu <mmughedd@c2r4s6.42london.com>
2024-03-10 08:53:24 +0000

    fixed env var issues

--- src/parser.c
+++ src/parser.c
@@ -6,69 +6,58 @@
 /*   By: mmughedd <mmughedd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 06:47:54 by mmughedd          #+#    #+#             */
-/*   Updated: 2024/03/09 14:14:12 by mmughedd         ###   ########.fr       */
+/*   Updated: 2024/03/10 08:52:44 by mmughedd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-
 char	*join_token(char *line, char *line_to_add)
 {
 	char	*tmp;
 
+	if (!line_to_add)
+		return(line);
 	tmp = ft_strjoin(line, line_to_add);
 	free(line);
 	return (tmp);
 }
 
-int	find_env_var(char *line, char *to_extend)
-{
-	int		len;
-	char	*tmp;
-
-	len = 1;
-	ft_strlcpy(tmp, to_extend, len);
-	while (!getenv(tmp) && to_extend[len])
-	{
-		free(tmp);
-		ft_strlcpy(tmp,	to_extend, ++len);
-	}
-	line = join_token(line, tmp);
-	free(tmp);
-	return (len);
-}
-
-// when tokenizing using a flag to check for pipes? 
-void	parser(t_token *token) // it can be changed to updated t_token
+void	parser(t_token *token)
 {
 	char	*line;
+	char	*tmp;
 	char	**extended;
 	int		i;
-	int		len;
 
 	if (!token)
 		return ; //empty input
 	line = malloc(sizeof(char) * (token->len + 1));
 	if (!line)
-		return ;//malloc error
-	i = 0;
+	{
+		return ;//malloc error, perror?
+	}
 	while (token)
 	{
+		i = 0;
 		if (token->type == 9) //pipe
 		{
 				// create process with line command
 				break;
 		}
-		else if (token->type <= 4)
+		else if (token->type <= 4) // strings, space, single and double quotes
 		{
-			if (token->type == 4 && ft_strchr(token->value, '$'))
+			if (token->type == 4 && ft_strchr(token->value, '$')) // is_extendable?
 			{
-				extended = ft_split(token->value, '$'); //what if $i is token->value[0]?
+				if (!ft_strnstr(token->value, "$", 1)) // check if first splitted word is env var
+					i = 1;
+				extended = ft_split(token->value, '$');
 				while (extended[i])
 				{
-					len = find_env_var(line, extended[i++]);
-					line = join_token(line, extended[len - 1]);
+					tmp = getenv(extended[i]);
+					line = join_token(line, tmp);
+					//free(tmp); TODO: free gives segv
+					i++;
 				}
 			}
 			else // no expand
@@ -76,15 +65,15 @@ void	parser(t_token *token) // it can be changed to updated t_token
 		}
 		token = token->next;
 	}
-	printf("%s", line);
+	printf("%s\n", line);
 }
 
-
+// for testing
 t_token *create_token(int type, char *value, int len)
 {
 	t_token *token;
-	token = malloc(sizeof(t_token));
 
+	token = malloc(sizeof(t_token));
 	token->type = type;
 	token->value = value;
 	token->len = len;
@@ -92,32 +81,47 @@ t_token *create_token(int type, char *value, int len)
 	return (token);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[], char *envp[])
 {
 	t_token *first;
-	char one[100];
-	strcpy(one, argv[1]);
-	char two[100];
-	strcpy(two, argv[2]);
-	char three[100];
-	strcpy(three, argv[3]);
+
+	char one[] = "$s$name$re"; // word, not extendable type 1
+	char two[] = " "; // space, type 2
+	char three[] = "$name"; //single var
+	char four[] = "  xxx$name$name"; //double var
+	char five[] = "$names"; // wrong var
+	char six[] = "$s$name$re"; // var between wrong vars
+	
 	first = create_token(1, one, 5);
-	first->next = create_token(2, two, 5);
+	first->next = create_token(2, two, 1);
 	first->next->next = create_token(4, three, 4);
+	first->next->next->next = create_token(4, four, 4);
+	first->next->next->next->next = create_token(4, five, 4);
+	first->next->next->next->next->next = create_token(4, six, 4);
 	parser(first);
 }
 
 // HOW TO TEST
 // cc -g parser.c ../libft/libft.a ../include/minishell.h
+// name = "John" ./a.out
 
-// name = "John"
+// to set env var in gdb
+// set environment name="john"
 
-// ./a.out "hello" " " "you"		returns "hello you"
+// no longer needed
+// int	find_env_var(char *line, char *to_extend)
+// {
+// 	int		len;
+// 	char	*tmp;
 
-// ./a.out "hello" " " "$name"		returns "hello John"
-
-// ./a.out "hello" " " "Mr $name"	returns "hello Mr John"
-
-// ./a.out "hello" " " "Mr $namesas"	returns "hello Mr "
-	//it should return "hello   Mr \n"
-//TODO: Understand why it expands regardless of type
+// 	len = 1;
+// 	ft_strlcpy(tmp, to_extend, len);
+// 	while (!getenv(tmp) && to_extend[len])
+// 	{
+// 		free(tmp);
+// 		ft_strlcpy(tmp,	to_extend, ++len);
+// 	}
+// 	line = join_token(line, tmp);
+// 	free(tmp);
+// 	return (len);
+// }
