3a0558c0bbc481875b5c7685fb6f3e79f14a6d16
Aleksandra Samuilkina <30261769+asamusam@users.noreply.github.com>
2024-03-15 15:42:48 +0000

    described the output of this section

--- include/parser.h
+++ include/parser.h
@@ -6,7 +6,7 @@
 /*   By: asamuilk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 15:00:30 by asamuilk          #+#    #+#             */
-/*   Updated: 2024/03/15 15:33:36 by asamuilk         ###   ########.fr       */
+/*   Updated: 2024/03/15 15:42:35 by asamuilk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 # include "libft.h"
 # include "minishell.h"
 
+// the executor gets a list of commands, i.e. t_list *commands.
+// one command - one part of the pipeline.
+// each node in the list of commands will contain a pointer to the structure below.
 typedef struct s_command
 {
-	t_list	*args; // list of arguments, where the first argument is a command, and the rest are args
+	t_list	*args; // list of arguments (the first argument is a command, rest are args)
 	char	*file_in; // file to get the input from
 	char	*file_out; // file to write the output to
 	int		file_out_flag;	// bit mask for file_out, so to check if it is for truncating, 
